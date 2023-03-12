bb84887941f45e9f743af71de3b7963675ede9ea
pacific.tang@gmail.com <pacific.tang@gmail.com>
2023-03-11 21:01:26 -0600

    reindex default sessions

--- decoding/train_EM.py
+++ decoding/train_EM.py
@@ -16,7 +16,7 @@ if __name__ == "__main__":
     parser.add_argument("--subject", type = str, required = True)
     parser.add_argument("--gpt", type = str, default = "perceived")
     parser.add_argument("--sessions", nargs = "+", type = int, 
-        default = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 13, 14, 17, 19])
+        default = [2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 14, 15, 18, 20])
     args = parser.parse_args()
 
     # training stories
